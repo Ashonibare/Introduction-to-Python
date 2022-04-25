0b32989ad9d1da40e695bb304fa2ac98a7b0eae8
Victor A. Animasahun <ashonibarevik@gmail.com>
2022-04-25 02:54:58 +0100

    Create 10-delete_items.py

---
+++ 10-delete_items.py
@@ -0,0 +1,9 @@
+# motorcycles: original list
+motorcycles = ['honda', 'yamaha', 'suzuki', 'bajaj']
+
+#delete items from your list
+del motorcycles[1]
+print(motorcycles)
+
+#code deletes item in index 1 (yamaha)
+# output = ['honda', 'suzuki', 'bajaj']
