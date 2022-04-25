63202532b213f6145462a1e2a33273b90d51bf37
Victor A. Animasahun <ashonibarevik@gmail.com>
2022-04-25 02:36:22 +0100

    Create 05-Concatenate_message_to_list.py

---
+++ 05-Concatenate_message_to_list.py
@@ -0,0 +1,8 @@
+names = ['Mayorkun', 'Skola', 'Francis', 'Ivy', 'Viktor', 'Vero', 'Jay']
+print(f"Hello, {names[0]}!")
+print(f"Hello, {names[1]}!")
+print(f"Hello, {names[2]}!")
+print(f"Hello, {names[3]}!")
+print(f"Hello, {names[4]}!")
+print(f"Hello, {names[5]}!")
+print(f"Hello, {names[6]}!")
