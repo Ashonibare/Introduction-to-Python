109a7637806108debf8bf9b18d6643287532ddd8
Victor A. Animasahun <ashonibarevik@gmail.com>
2022-04-25 02:41:59 +0100

    Create 07-modiy_list_element.py

---
+++ 07-modiy_list_element.py
@@ -0,0 +1,10 @@
+#working with lists
+motorcycles = ['honda', 'yamaha', 'suzuki', 'bajaj']
+
+#modifying elements in a list
+#adding values
+motorcycles[0] = 'kantra'
+motorcycles.append('haiju')
+print(motorcycles)
+
+#output will be ['kantra', 'yamaha', 'suzuki', 'bajaj', 'haiju']
