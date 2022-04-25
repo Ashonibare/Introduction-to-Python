28f61b15c4364b583daf2ace3e46337c106ff5b5
Victor A. Animasahun <ashonibarevik@gmail.com>
2022-04-25 02:44:28 +0100

    Create 08-dramatically_add_items.py

---
+++ 08-dramatically_add_items.py
@@ -0,0 +1,9 @@
+# motorcycles: original list
+motorcycles = ['honda', 'yamaha', 'suzuki', 'bajaj']
+print(motorcycles[3])
+
+#dramatically add items to your list
+motorcycles2 = []
+motorcycles2.append('alau')
+motorcycles2.append('timo')
+motorcycles2.append('daylong')
