b50dc29bdf0ec801ba95386096bcd87bce562304
Victor A. Animasahun <ashonibarevik@gmail.com>
2022-04-26 16:00:08 +0100

    Create 11-delete_item_from_rear.py

---
+++ 11-delete_item_from_rear.py
@@ -0,0 +1,8 @@
+#morcyles = list of motorcycles
+motorcycles2 = ['honda', 'yamaha', 'suzuki', 'bajaj']
+
+#delete an item from the rear
+motorcycles2.pop()
+print(motorcycles2)
+
+#product will be ['honda', 'yamaha', 'suzuki']
